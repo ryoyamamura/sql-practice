f4460d805125bca3632e96de4ca12c415bc66a4e
ryoyamamura <120030789+ryoyamamura@users.noreply.github.com>
2024-06-04 14:12:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ import matplotlib.pyplot as plt
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, text
 
-import os
-from dotenv import load_dotenv
-
-# .envファイルの内容を読み込む
-load_dotenv()
-
 # 環境変数の取得
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
