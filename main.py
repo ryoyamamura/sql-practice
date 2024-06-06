d4862e2feb24e86ec4d13e474eb45848825d7cf6
ryoyamamura <120030789+ryoyamamura@users.noreply.github.com>
2024-06-06 09:18:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ import streamlit as st
 from pygwalker.api.streamlit import StreamlitRenderer
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
-
 import os
-from dotenv import load_dotenv
-
-# .envファイルの内容を読み込む
-load_dotenv()
 
 # 環境変数の取得
 db_user = os.getenv('DB_USER')
